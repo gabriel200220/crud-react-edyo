50360833994dd89e435e14fd631e0be005771314
LuanFelipeCB <felipe14dacruz@hotmail.com>
2022-10-26 22:30:06 -0300

    Co-authored-by: Matheus Mariano de Camargo <MMCamargo@users.noreply.github.com> Co-authored-by: Leonardo Duarte <EagleDuarte@users.noreply.github.com>

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,17 +1,55 @@
 import { Container, Button, Paper, TextField } from '@mui/material'
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { FormEventHandler, useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import User from '../../shared/interfaces/Interfaces'
 import { containerStyled, paperStyled } from '../../shared/styles/Styles'
 
 function Register() {
-	const [nome, setNome] = useState('')
-	const [email, setEmail] = useState('')
-	const [senha, setSenha] = useState('')
-	const [reSenha, setReSenha] = useState('')
+	const navigate = useNavigate()
 
-	setInterval(() => {
-		console.log(nome)
-	}, 1000)
+	const userList = JSON.parse(
+		localStorage.getItem('userList') || '[]'
+	) as User[]
+
+	const onSubmit = (e: any) => {
+		e.preventDefault()
+
+		validateRegister(e)
+
+		const user = {
+			name: e.target.elements.name.value,
+			email: e.target.elements.email.value,
+			password: e.target.elements.password.value
+		}
+
+		userList.push(user)
+
+		localStorage.setItem('userList', JSON.stringify(userList))
+
+		navigate('/')
+	}
+
+	const validateRegister = (e: any) => {
+		if (!e.target.elements.name.value || e.target.elements.email.value) {
+			alert('Verifique os campos!')
+			return
+		}
+
+		// if (user.password !== user.repassword) {
+		// 	alert("Campos não coincidem!")
+		// 	return;
+		// }
+	}
+
+	const verifyUser = (e: any) => {
+		const verified = userList.find(
+			(user: User) => user.email === user.email
+		)
+
+		if (!verified) {
+			alert('Usuário existente!')
+		}
+	}
 
 	return (
 		<Container sx={containerStyled}>
@@ -19,31 +57,29 @@ function Register() {
 				<header>
 					<h2>Página de Cadastro</h2>
 				</header>
-				<TextField
-					type='text'
-					placeholder='Digite seu nome'
-					onChange={(e) => setNome(e.target.value)}
-					value={nome}
-				/>
-				<TextField
-					type='text'
-					placeholder='Digite seu e-mail'
-					onChange={(e) => setEmail(e.target.value)}
-					value={email}
-				/>
-				<TextField
-					type='password'
-					placeholder='Digite sua senha'
-					onChange={(e) => setSenha(e.target.value)}
-					value={senha}
-				/>
-				<TextField
-					type='password'
-					placeholder='Digite sua senha'
-					onChange={(e) => setResenha(e.target.value)}
-					value={reSenha}
-				/>
-				<Button>Cadastrar</Button>
+				<form onSubmit={onSubmit}>
+					<TextField
+						name='name'
+						type='text'
+						placeholder='Digite seu nome'
+					/>
+					<TextField
+						name='email'
+						type='text'
+						placeholder='Digite seu e-mail'
+					/>
+					<TextField
+						name='password'
+						type='password'
+						placeholder='Digite sua senha'
+					/>
+					<TextField
+						name='repassword'
+						type='password'
+						placeholder='Digite sua senha'
+					/>
+					<Button type='submit'>Cadastrar</Button>
+				</form>
 				<footer>
 					<p>
 						Já possui uma conta? <Link to={'/'}>Conecte-se!</Link>
